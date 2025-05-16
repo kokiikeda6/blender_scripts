75a0bb9c57c4ef0eae462e8f9f92b9e831f4b2ae
kokiikeda6 <s21c1010gm@s.chibakoudai.jp>
2025-05-16 18:10:53 +0900

    4方向の障害物間距離計測

--- pepper_model_test/obst_dist_measure.py
+++ pepper_model_test/obst_dist_measure.py
@@ -90,23 +90,43 @@ def obst_dist_measure(bm, world_cicle, target_z):
     center_point_2d = Vector((center_x, center_y))
 
     # 指定した高さにある頂点を収集し、一方向のみに絞る
-    vertices_left_side = []
+    vertices_left = []
+    vertices_right = []
+    vertices_front = []
+    vertices_behind = []
+
     for v in bm.verts:
         if abs(v.co.z - target_z) < TOLERANCE: #高さ絞る
             point_2d = Vector((v.co.x, v.co.y))
             if (point_2d - center_point_2d).length > radius + 0.01: # 検知した円より外の点に絞る，0.01は要調整
                 if v.co.x > center_x and v.co.y < center_y + radius and v.co.y > center_y - radius: # 円の左側に絞る
-                    vertices_left_side.append(v.co)
-    
-    if not vertices_left_side:
-        print("No vertices found near the base point at the target height.")
-        bm.free()
-        return
+                    vertices_left.append(v.co)
+                if v.co.x < center_x and v.co.y < center_y + radius and v.co.y > center_y - radius: # 円の右側に絞る
+                    vertices_right.append(v.co)
+                if v.co.y > center_y and v.co.x < center_x + radius and v.co.x > center_x - radius: # 円の前側に絞る
+                    vertices_front.append(v.co)
+                if v.co.y < center_y and v.co.x < center_x + radius and v.co.x > center_x - radius: # 円の後側に絞る
+                    vertices_behind.append(v.co)
     
     # 最小の距離を求める
-    min_dist = min([abs(center_x - v.x) for v in vertices_left_side])
+    if not vertices_left:
+        left_obst_dist = None
+    else:
+        left_obst_dist = min([abs(center_x - v.x) for v in vertices_left])
+    if not vertices_right:
+        right_obst_dist = None
+    else:
+        right_obst_dist = min([abs(center_x - v.x) for v in vertices_right])
+    if not vertices_front:
+        front_obst_dist = None
+    else:
+        front_obst_dist = min([abs(center_y - v.y) for v in vertices_front])
+    if not vertices_behind:
+        behind_obst_dist = None
+    else:
+        behind_obst_dist = min([abs(center_y - v.y) for v in vertices_behind])
 
-    return min_dist
+    return [left_obst_dist, right_obst_dist, front_obst_dist, behind_obst_dist]
 
 def main():
 
@@ -137,7 +157,11 @@ def main():
     obst_dist = obst_dist_measure(bm, world_circle, target_z)
 
     cv2.putText(image, f"x= {world_circle[0]}, y= {world_circle[1]}, r= {world_circle[2]}", (0, 90), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
-    cv2.putText(image, f"left_obst_dist= {obst_dist}", (0, 190), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
+    cv2.putText(image, f"left_obst_dist= {obst_dist[0]}", (0, 140), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
+    cv2.putText(image, f"right_obst_dist= {obst_dist[1]}", (0, 190), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
+    cv2.putText(image, f"front_obst_dist= {obst_dist[2]}", (0, 240), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
+    cv2.putText(image, f"behind_obst_dist= {obst_dist[3]}", (0, 290), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255,255,255))
+
 
     # 一時ファイルに画像を保存
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
